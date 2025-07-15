f1f6bcc4e843da4f404e430d32f226e4d6927411
Mujaca <mujaca0@gmail.com>
2025-07-15 16:55:45 +0200

    shards testing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-
+	<div>
+		<Shard id="1" name="Cod" image="cod" />
+		<div class="dropzone" @drop="onDrop" @dragover.prevent>Drop-Ziel</div>
+	</div>
 </template>
 
 <script setup lang="ts">
+import Shard from './components/Shard.vue';
+
+function onDrop(event: DragEvent) {
+	event.preventDefault();
+	const data = event.dataTransfer?.getData('text/plain');
+	console.log('Gedroppt:', data);
+}
+
 </script>
 
 
 <style lang="scss" scoped>
+.dropzone {
+	width: 32px;
+	height: 32px;
+	padding: 4px;
+	background-color: #4a4a4a;
+	border-radius: 8px;
 
+}
 </style>
---
+++ src/components/Shard.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="shard" draggable="true" @dragstart="onDragStart">
+		<img draggable="false" :src="`/public/images/shards/${image}.png`" :alt="name" />
+	</div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+	id: string;
+	name: string;
+	image: string;
+}>();
+
+function onDragStart(event: DragEvent) {
+	event.dataTransfer?.setData('text/plain', props.id);
+}
+
+</script>
+
+<style lang="scss" scoped>
+.shard {
+
+	width: 32px;
+	height: 32px;
+
+	padding: 4px;
+
+	background-color: #4a4a4a;
+	border-radius: 8px;
+
+	transition: background-color 0.2s ease-in-out;
+
+	&:hover {
+		//cursor: pointer;
+		background-color: #595959;
+	}
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+}
+</style>
