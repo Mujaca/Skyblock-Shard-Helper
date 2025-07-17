7274cbc1f33e7599a97d6431b1cdfb09b9133b6b
Mujaca <mujaca0@gmail.com>
2025-07-17 14:40:55 +0200

    added filter placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
 	<div class="main-container">
-		<div class="shard-list">
-			<Shard
-				v-for="shard in shardStore.getShardArray"
-				:key="shard.productID"
-				:id="shard.productID" />
+		<div class="shard-area">
+			<div class="filter">
+				<input placeholder="Shard name">
+				<select>
+					<option value="all">All Rarities</option>
+					<option value="common">Common</option>
+					<option value="uncommon">Uncommon</option>
+					<option value="rare">Rare</option>
+					<option value="epic">Epic</option>
+					<option value="legendary">Legendary</option>
+				</select>
+				<select>
+					<option value="all">All Families</option>
+				</select>
+				<select>
+					<option value="all">All Types</option>
+				</select>
+				<input type="checkbox" id="showFusions">
+				<label for="showFusions">Show base shards</label>
+			</div>
+			<div class="shard-list">
+				<Shard v-for="shard in shardStore.getShardArray" :key="shard.productID" :id="shard.productID" />
+			</div>
 		</div>
 		<div class="fusion-area">
 			<div class="input-area">
-				<Dropzone/>
+				<Dropzone />
 			</div>
 			<p class="direction-indicator">
 				<img :src="arrowRight">
 			</p>
 			<div class="output-area">
-				<OutputShard
-				v-if="shardStore.fusionResults.length > 0"
-				v-for="result in shardStore.fusionResults"
-				:possibleResults="result" />
+				<OutputShard v-if="shardStore.fusionResults.length > 0" v-for="result in shardStore.fusionResults"
+					:possibleResults="result" />
 			</div>
 		</div>
 	</div>
@@ -36,7 +52,6 @@ const shardStore = useShardStore();
 </script>
 
 <style lang="scss" scoped>
-
 .main-container {
 	display: flex;
 	flex-direction: row;
@@ -44,17 +59,45 @@ const shardStore = useShardStore();
 	height: 100%;
 }
 
-.shard-list {
+.shard-area {
 	width: 45%;
 	display: flex;
-	flex-wrap: wrap;
-	gap: 8px;
-	overflow-y: auto;
-	overflow-x: visible;
-	position: relative;
-	height: 100%;
-	max-height: 90vh;
+	flex-direction: column;
+	gap: 1rem;
+
+	.filter {
+
+		display: flex;
+		gap: 0.5rem;
+		align-items: center;
+
+		input,
+		select {
+			padding: 0.5rem;
+			border: 1px solid #ccc;
+			border-radius: 4px;
+			font-size: 1rem;
+		}
+
+		input[type="checkbox"] {
+			width: 1.5rem;
+			height: 1.5rem;
+			margin-left: auto;
+		}
+	}
+
+	.shard-list {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 8px;
+		overflow-y: auto;
+		overflow-x: visible;
+		position: relative;
+		height: 100%;
+		max-height: 90vh;
+	}
 }
+
 
 .fusion-area {
 	width: 25%;
@@ -67,7 +110,7 @@ const shardStore = useShardStore();
 	grid-template-rows: .5fr 1fr .5fr;
 	place-items: center;
 
- 	.input-area {
+	.input-area {
 		grid-column: 1;
 		grid-row: 2;
 	}
