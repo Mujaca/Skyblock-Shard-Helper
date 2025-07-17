2db561854178921a08b12105f23184a5991bbd06
Mujaca <mujaca0@gmail.com>
2025-07-17 14:31:05 +0200

    added svg icon and placed the fusion area

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
 			<div class="input-area">
 				<Dropzone/>
 			</div>
+			<p class="direction-indicator">
+				<img :src="arrowRight">
+			</p>
 			<div class="output-area">
 				<OutputShard
 				v-if="shardStore.fusionResults.length > 0"
@@ -21,6 +24,8 @@
 </template>
 
 <script setup lang="ts">
+import arrowRight from './assets/images/arrow-right-solid.svg';
+
 import Dropzone from './components/Dropzone.vue';
 import OutputShard from './components/OutputShard.vue';
 import Shard from './components/Shard.vue';
@@ -52,9 +57,40 @@ const shardStore = useShardStore();
 }
 
 .fusion-area {
-	display: flex;
-	flex-direction: column;
-	width: 55%;
+	width: 25%;
 	height: 100%;
+	min-height: 90vh;
+	margin-left: auto;
+	margin-right: auto;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-template-rows: .5fr 1fr .5fr;
+	place-items: center;
+
+ 	.input-area {
+		grid-column: 1;
+		grid-row: 2;
+	}
+
+	.direction-indicator {
+		grid-row: 2;
+		grid-column: 2;
+		width: 96px;
+		height: 96px;
+	}
+
+	.output-area {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		grid-column: 3;
+		grid-row: 2;
+	}
+
+	img {
+		filter: invert(1);
+		max-width: 100%;
+		max-height: 100%;
+	}
 }
 </style>
